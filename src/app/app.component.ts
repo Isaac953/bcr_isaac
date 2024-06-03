b8906850c9a73cb02f0718f9bfdf20e9c72626fc
Isaac Arias <isaac.arias@bcr.gob.sv>
2024-06-03 11:44:36 -0600

    Nuevo proyecto
    Probando crear nuevo proyecto con Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DropdownModule, FormsModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'preguntas_cspro';
+export class AppComponent implements OnInit {
+  selectedCity: any;
+  value: any;
+
+  title = 'BCR Isaac';
+
+  cities!: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+
+    this.cities = [
+      { name: 'City 1', value: 'city1' },
+      { name: 'City 2', value: 'city2' },
+    ]
+  }
 }
